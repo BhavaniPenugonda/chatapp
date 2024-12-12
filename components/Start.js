7135b7b8b1309c172d2727e9ebde0ec9fc853244
BhavaniPenugonda <bhavani0202@gmail.com>
2024-12-12 16:24:40 +0100

    Background image and App title is added

--- components/Start.js
+++ components/Start.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Text, Button,TextInput } from 'react-native';
+import { StyleSheet, View, Text, Button,TextInput,ImageBackground } from 'react-native';
 import { useState } from 'react';
 
 
@@ -9,6 +9,8 @@ const Start= ({navigation})=>{
   return (
 
     <View style={styles.container}>
+      <ImageBackground source={require('./assets/BackgroundImage.png')} resizeMode="cover" style={styles.backgroundImage}></ImageBackground>
+      <Text style={styles.title}>ChatApp</Text>
       <TextInput
         style={styles.textInput}
         value={name}
@@ -35,7 +37,23 @@ const styles = StyleSheet.create({
     padding: 15,
     borderWidth: 1,
     marginTop: 15,
-    marginBottom: 15
+    marginBottom: 15,
+    color: '#757083',
+    fontSize: 16, 
+    fontWeight: '300', 
+    opacity: 50,
+  },
+  backgroundImage: {
+    flex: 1,  
+    alignItems: 'center',
+    justifyContent: 'center', 
+    height: '100%',
+    width: '100%',
+  },
+  title:{
+    fontSize :45, 
+    fontWeight :600, 
+    fontColor :'#FFFFFF'
   }
  });
  
