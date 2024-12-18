2ee01b26586c7076f986df9dfc233a6710cab0dc
BhavaniPenugonda <bhavani0202@gmail.com>
2024-12-18 14:33:04 +0100

    messages setter function is added

--- components/Chat.js
+++ components/Chat.js
@@ -6,6 +6,21 @@ import { GiftedChat } from "react-native-gifted-chat";
 const Chat = ({ route,navigation }) => {
   const [messages, setMessages] = useState([]);
   const { name, backgroundColor } = route.params;
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+    ]);
+  }, []);
   // Set the navigation title dynamically based on the name
   useEffect(() => {
     navigation.setOptions({ title: name });
