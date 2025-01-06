6e119f99d1216c739c9a4285a425d3f0feda66e6
BhavaniPenugonda <bhavani0202@gmail.com>
2025-01-06 14:11:04 +0100

    imported the dependency async storage

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import Chat from "./components/Chat";
 import Start from "./components/Start";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
