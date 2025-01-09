478d5838e1094d3305236ed71f6eaefad230d4f7
BhavaniPenugonda <bhavani0202@gmail.com>
2025-01-09 15:18:29 +0100

    custom actions component is defined

---
+++ components/CustomActions.js
@@ -0,0 +1,13 @@
+import { TouchableOpacity } from "react-native";
+
+const CustomActions = () => {
+
+  return (
+    <TouchableOpacity style={styles.container} >
+      
+    </TouchableOpacity>
+    
+  );
+}
+
+export default CustomActions;
