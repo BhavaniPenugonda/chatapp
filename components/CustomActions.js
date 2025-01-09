b39e3c4e434a72218d2d22aeb09c50faec03c36b
BhavaniPenugonda <bhavani0202@gmail.com>
2025-01-09 15:21:20 +0100

    Inside the view text is added

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -2,9 +2,13 @@ import { TouchableOpacity } from "react-native";
 
 const CustomActions = () => {
 
+  const onActionPress = () => {}
+
   return (
-    <TouchableOpacity style={styles.container} >
-      
+    <TouchableOpacity style={styles.container} onPress={onActionPress}>
+    <View style={[styles.wrapper, wrapperStyle]}>
+        <Text style={[styles.iconText, iconTextStyle]}>+</Text>
+      </View>
     </TouchableOpacity>
     
   );
