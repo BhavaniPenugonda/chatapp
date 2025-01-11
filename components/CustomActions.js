cedd024b636c5b5b044b0a773edddbc3712cfbc6
BhavaniPenugonda <bhavani0202@gmail.com>
2025-01-11 12:47:23 +0100

    console statement is added

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -80,6 +80,7 @@ const CustomActions = ({ wrapperStyle, iconTextStyle, onSend,storage,userID}) =>
     const blob = await response.blob();
     uploadBytes(newUploadRef, blob).then(async (snapshot) => {
       const imageURL = await getDownloadURL(snapshot.ref)
+      console.log("Image URL: ", imageURL);
       onSend({ image: imageURL })
     });
   }
