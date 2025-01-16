84a738089b94aae3b5c3f465d3882a6f0739b94b
BhavaniPenugonda <bhavani0202@gmail.com>
2025-01-16 16:16:46 +0100

    Added image and location

--- components/Chat.js
+++ components/Chat.js
@@ -42,8 +42,10 @@ const Chat = ({ db,route,navigation,isConnected ,storage}) => {
      newMessages.push({
        id: doc.id,
        ...doc.data(),
-       createdAt: new Date(doc.data().createdAt.toMillis())
-       
+       createdAt: new Date(doc.data().createdAt.toMillis()),
+       user: data.user,
+       image: data.image || null,
+       location: data.location || null,
      })
    })
    cacheMessages(newMessages);
@@ -106,7 +108,7 @@ const onSend = (newMessages) => {
    }
 
    const renderCustomActions = (props) => {
-    return <CustomActions userID={userID} onSend={onSend} storage={storage} {...props} />;
+    return <CustomActions userID={userID}  storage={storage} {...props} />;
   };
   
   const renderCustomView = (props) => {
